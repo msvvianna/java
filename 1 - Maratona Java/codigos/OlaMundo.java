3b0d2a5348d6ae49dd7e6915cc1832a7e37a5141
marcos <msvvianna@gmail.com>
2025-02-20 12:48:53 -0300

    Finalizado os estudos do Dia 1

---
+++ 1 - Maratona Java/codigos/OlaMundo.java
@@ -0,0 +1,6 @@
+public class OlaMundo {
+	public static void main (String[] args){
+		System.out.println("KA-ME-HA-ME-HAAAAA");
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# 20/02/2025 - Inicio do curso de Java.
+
+
+
+
+
